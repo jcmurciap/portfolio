a7df7bef8d98c0d9a7f071c9f2b43a40a0a33286
enigma <camilo.murcia.pimentel@gmail.com>
2022-01-07 17:23:22 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './styles.css';
+import { AboutMe } from './AboutMe';
+import { ContactMe } from './ContactMe';
+import { DarkMode } from './DarkMode';
+import { Footer } from './Footer';
+import { Header } from './Header';
+import { Projects } from './Projects';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => {
+    return (
+        <>
+            <DarkMode />
+            <div className='container'>
+                <Header className="header"/>
+                <AboutMe className="about"/>
+                <ContactMe className="contact"/>
+                <Projects className="projects"/>
+                <Footer className="footer"/>
+            </div>
+        </>
+    );
+};
