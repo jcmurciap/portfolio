504da6d3196ee531b09902ec1e9436410e0dfdc5
enigma <camilo.murcia.pimentel@gmail.com>
2022-01-07 22:28:30 -0500

    Add biography text and its title

---
+++ src/AboutMe.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const AboutMe = () => {
+    return (
+        <div className='description'>
+            <h1 className='description-title'>About me</h1>
+            <p>Hello there! My name is Juan Camilo. I'm FrontEnd and Industrial enginner.
+                Before now, I worked at Rappi and Sport Life like technology analyst, and 
+                graduated (in progress) from ECCI University. I currently make a lot of 
+                mini-projects and putting on ideas to get more confident about software 
+                development specially to frontEnd side, however I also do things on the 
+                server side like a <em>CRUD</em> using <em>MERN</em> stack.<br/>
+                <br/><em>I'm self though developer</em> because that the moment when I discovered programming
+                language I fell 
+                in love with it and I will not change my mind for at least the next 80 years.
+                 I think that I could be a key piece in your organization because I really 
+                like real challenges, I learn quickly and I like to work as a team.
+            </p>
+        </div>
+    )
+}
