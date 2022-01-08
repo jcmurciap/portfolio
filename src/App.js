4b10d9bcc9c087b54b3eb8b4ec4066b6ba29d1ed
enigma <camilo.murcia.pimentel@gmail.com>
2022-01-08 14:32:31 -0500

    chore postcss config

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './styles.css';
 import { AboutMe } from './AboutMe';
 import { ContactMe } from './ContactMe';
 import { DarkMode } from './DarkMode';
-import { Footer } from './Footer';
 import { Header } from './Header';
 import { Projects } from './Projects';
 
@@ -12,12 +11,11 @@ export const App = () => {
         <>
             <DarkMode />
             <div className='container'>
-                <Header className="header"/>
-                <AboutMe className="about"/>
-                <ContactMe className="contact"/>
-                <Projects className="projects"/>
-                <Footer className="footer"/>
-            </div>
+                <Header />
+                <AboutMe />
+                <ContactMe />
+                <Projects />
+            </div> 
         </>
     );
 };
