4608ca2273ceea95b7674b158af464a613a7de27
enigma <camilo.murcia.pimentel@gmail.com>
2022-01-08 20:29:05 -0500

    Add mobile version

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -9,10 +9,18 @@ export const ContactMe = () => {
         <div>
             <h1 className='description-title'>Contact me</h1>
             <div className='logos'>
-                <img className='logo' src={github} alt="github logo" width="70" height="70"/>
-                <img className='logo' src={linkedin} alt="linkedin logo" width="70" height="70"/>
-                <img className='logo' src={gmail} alt="gmail logo" width="70" height="70"/>
-                <img className='logo' src={twitter} alt="twitter logo" width="70" height="70"/>
+                <a href="https://github.com/jcmurciap">
+                    <img className='logo' src={github} alt="github logo" width="70" height="70"/>
+                </a>
+                <a href="https://www.linkedin.com/in/juan-murcia-p-91b033208/">
+                    <img className='logo' src={linkedin} alt="linkedin logo" width="70" height="70"/>
+                </a>
+                <a href="https://mail.google.com/mail/u/0/#inbox">
+                    <img className='logo' src={gmail} alt="gmail logo" width="70" height="70"/>
+                </a>
+                <a href="https://twitter.com/jc_murcia">
+                    <img className='logo' src={twitter} alt="twitter logo" width="70" height="70"/>
+                </a>
             </div>
         </div>
     );
--- src/Projects.js
+++ src/Projects.js
@@ -20,46 +20,46 @@ export const Projects = () => {
                 <div className='projects'>
                     <div className='project'>
                         <img className='project-image' src={project1} alt="development project" width={200} height={150}/>
-                        <h4 className='project-title'>Project title</h4>
+                        <h4 className='project-title'>TODO List App</h4>
                         <aside>
                             <small className='project-title'>Tools used</small>
                             <div className='tools'>
-                                <img src={js} alt="javascript logo" width={30} height={30} />
-                                <img src={ts} alt="typescript logo" width={30} height={30} />
-                                <img src={html} alt="html logo" width={30} height={30} />
-                                <img src={css} alt="css logo" width={30} height={30} />
-                                <img src={git} alt="git logo" width={40} height={30} />
-                                <img src={react} alt="react logo" width={100} height={30} />
+                                <img src={js} alt="javascript logo" width={18} height={18} />
+                                <img src={ts} alt="typescript logo" width={18} height={18} />
+                                <img src={html} alt="html logo" width={20} height={20} />
+                                <img src={css} alt="css logo" width={20} height={20} />
+                                <img src={git} alt="git logo" width={40} height={20} />
+                                <img src={react} alt="react logo" width={80} height={20} />
                             </div>
                         </aside>
                     </div>
                     <div className='project'>
                         <img className='project-image' src={project2} alt="development project" width={200} height={150}/>
-                        <h4 className='project-title'>Project title</h4>
+                        <h4 className='project-title'>Calendar App</h4>
                         <aside>
                             <small className='project-title'>Tools used</small>
                             <div className='tools'>
-                                <img src={js} alt="javascript logo" width={30} height={30} />
-                                <img src={ts} alt="typescript logo" width={30} height={30} />
-                                <img src={html} alt="html logo" width={30} height={30} />
-                                <img src={css} alt="css logo" width={30} height={30} />
-                                <img src={git} alt="git logo" width={40} height={30} />
-                                <img src={react} alt="react logo" width={100} height={30} />
+                                <img src={js} alt="javascript logo" width={18} height={18} />
+                                <img src={ts} alt="typescript logo" width={18} height={18} />
+                                <img src={html} alt="html logo" width={20} height={20} />
+                                <img src={css} alt="css logo" width={20} height={20} />
+                                <img src={git} alt="git logo" width={40} height={20} />
+                                <img src={react} alt="react logo" width={80} height={20} />
                             </div>
                         </aside>
                     </div>
                     <div className='project'>
                         <img className='project-image' src={project3} alt="development project" width={200} height={150}/>
-                        <h4 className='project-title'>Project title</h4>
+                        <h4 className='project-title'>Marvel Heroes App</h4>
                         <aside>
                             <small className='project-title'>Tools used</small>
                             <div className='tools'>
-                                <img src={js} alt="javascript logo" width={30} height={30} />
-                                <img src={ts} alt="typescript logo" width={30} height={30} />
-                                <img src={html} alt="html logo" width={30} height={30} />
-                                <img src={css} alt="css logo" width={30} height={30} />
-                                <img src={git} alt="git logo" width={40} height={30} />
-                                <img src={react} alt="react logo" width={100} height={30} />
+                                <img src={js} alt="javascript logo" width={18} height={18} />
+                                <img src={ts} alt="typescript logo" width={18} height={18} />
+                                <img src={html} alt="html logo" width={20} height={20} />
+                                <img src={css} alt="css logo" width={20} height={20} />
+                                <img src={git} alt="git logo" width={40} height={20} />
+                                <img src={react} alt="react logo" width={80} height={20} />
                             </div>
                         </aside>
                     </div>
